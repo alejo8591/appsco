5b6773ff0d03a958fa56bcd1374d8a02f515ce09
Alejandro Romero <alejo8591@gmail.com>
2013-03-31 18:31:43 -0500

    first commit with bootcamps appsco

---
+++ ror/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c021e3c9487f6c08e1160ec8ae99581dc95a4edf2b4888ef52c69892e1fb38fa64d00c2ab9d185a89dba1b8ce169c2b4470496ef11fab2bb1635e36edf8ad188'
